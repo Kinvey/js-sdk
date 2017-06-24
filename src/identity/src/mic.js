82855826c15fd5cd97080c84f437b63129f70a15
Thomas Conner <thomas@kinvey.com>
2017-06-23 22:06:22 -0400

    Fix incorrect MIC url

--- src/identity/src/mic.js
+++ src/identity/src/mic.js
@@ -120,7 +120,7 @@ export class MobileIdentityConnect extends Identity {
           version = String(version);
         }
 
-        pathname = urljoin(pathname, version.indexOf('v') === 0 ? version : `v${version}`);
+        pathname = urljoin(version.indexOf('v') === 0 ? version : `v${version}`, pathname);
       }
 
       return popup.open(url.format({
