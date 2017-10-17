42a82c8dd7acfabe1295cc5cf55a62454e095c0a
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-10-17 10:33:57 +0300

    Added tests for Find

---
+++ test/integration/CRUD Entity/crud-network-store.test.js
@@ -0,0 +1,136 @@
+runner.run(testFunc);
+
+function testFunc() {
+
+    const uid = (size = 10) => {
+        let text = '';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        for (let i = 0; i < size; i += 1) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text;
+    }
+
+    const randomString = (size = 18, prefix = '') => {
+        return `${prefix}${uid(size)}`;
+    }
+
+    const assertEntityMetadata = (entity) => {
+
+        expect(entity._kmd.lmt).to.exist;
+        expect(entity._kmd.ect).to.exist;
+        expect(entity._acl.creator).to.exist;
+    }
+
+    const cleanCollectionData = (collectionName, done) => {
+        const store = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
+        return store.find().toPromise()
+        .then((entities) => {
+            async.each(entities.map(a => a._id), (entityId, callback) => {
+                const query = new Kinvey.Query();
+                query.equalTo('_id', entityId);
+                return store.remove(query)
+                    .then(callback)
+            }, () => {
+                done();
+            });
+        }).catch(done);
+
+    }
+
+    describe('Network Store', function() {
+
+        const collectionName = 'Books';
+
+        before((done) => {
+            Kinvey.initialize({
+                appKey: externalConfig.appKey,
+                appSecret: externalConfig.appSecret
+            });
+
+            Kinvey.User.signup()
+                .then(() => {
+                    cleanCollectionData(collectionName, done)
+                })
+        });
+        describe('Find', function() {
+
+            const store = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
+            const entity1 = {
+                _id: randomString()
+            };
+            const entity2 = {
+                _id: randomString()
+            };
+
+            before((done) => {
+                        return store.save(entity1)
+                    .then(() => {
+                        return store.save(entity2)
+                    })
+                    .then(() => {
+                        done();
+                    })
+            });
+
+        describe('find()', function() {
+            it('should throw an error if the query argument is not an instance of the Query class', function(done) {
+                store.find({})
+                    .subscribe(null, (error) => {
+                        try {
+                            expect(error.message).to.equal('Invalid query. It must be an instance of the Query class.');
+                            done();
+                        } catch (e) {
+                            done(e);
+                        }
+                    });
+            });
+
+            it('should return all the entities from the backend', (done) => {
+                return store.find().toPromise()
+                    .then((entities) => {
+                        expect(entities).to.be.an('array');
+                        expect(entities.length).to.equal(2);
+                        assertEntityMetadata(_.find(entities, { '_id': entity1._id}));
+                        assertEntityMetadata(_.find(entities, { '_id': entity2._id}));
+                        done();
+                    }).catch(done);
+            });
+
+            it('should find the entities that match the query', (done) => {
+                const query = new Kinvey.Query();
+                query.equalTo('_id', entity2._id);
+                return store.find(query).toPromise()
+                .then((entity) => {
+                    expect(entity).to.be.an('array');
+                    expect(entity.length).to.equal(1);
+                    assertEntityMetadata(_.find(entity, { '_id': entity2._id}));
+                    done();
+                }).catch(done);
+            });
+        });
+
+        describe('findById()', function () {
+            it('should throw a NotFoundError if the id argument does not exist', (done) => {
+                const entityId = randomString();
+                return store.findById(entityId).toPromise()
+                .catch((error) => {
+                    expect(error.message).to.contain('This entity not found in the collection');
+                    done();
+                }).catch(done);
+            });
+        
+            it('should return the entity that matches the id argument', (done) => {
+                return store.findById(entity2._id).toPromise()
+                .then((entity) => {
+                    expect(entity._id).to.equal(entity2._id)
+                    assertEntityMetadata(entity);
+                    done();
+                }).catch(done);
+            });
+          });
+        });
+    });
+}
