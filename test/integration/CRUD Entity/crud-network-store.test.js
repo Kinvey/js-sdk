dab0f062f17d4b9357a856d2fb20c65be7f30b4b
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-10-17 11:00:40 +0300

    Added tests for count

--- test/integration/CRUD Entity/crud-network-store.test.js
+++ test/integration/CRUD Entity/crud-network-store.test.js
@@ -2,6 +2,8 @@ runner.run(testFunc);
 
 function testFunc() {
 
+    const invalidQueryMessage = 'Invalid query. It must be an instance of the Query class.';
+
     const uid = (size = 10) => {
         let text = '';
         const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -37,14 +39,21 @@ function testFunc() {
                 done();
             });
         }).catch(done);
-
     }
 
     describe('Network Store', function() {
 
         const collectionName = 'Books';
+        const store = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
+        const entity1 = {
+            _id: randomString()
+        };
+        const entity2 = {
+            _id: randomString()
+        };
 
         before((done) => {
+            
             Kinvey.initialize({
                 appKey: externalConfig.appKey,
                 appSecret: externalConfig.appSecret
@@ -54,33 +63,23 @@ function testFunc() {
                 .then(() => {
                     cleanCollectionData(collectionName, done)
                 })
+                .then(() => {
+                    return store.save(entity1)
+                })
+                .then(() => {
+                    return store.save(entity2)
+                })
+                .then(() => {
+                    done();
+                })
         });
-        describe('Find', function() {
-
-            const store = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
-            const entity1 = {
-                _id: randomString()
-            };
-            const entity2 = {
-                _id: randomString()
-            };
-
-            before((done) => {
-                        return store.save(entity1)
-                    .then(() => {
-                        return store.save(entity2)
-                    })
-                    .then(() => {
-                        done();
-                    })
-            });
 
         describe('find()', function() {
             it('should throw an error if the query argument is not an instance of the Query class', function(done) {
                 store.find({})
                     .subscribe(null, (error) => {
                         try {
-                            expect(error.message).to.equal('Invalid query. It must be an instance of the Query class.');
+                            expect(error.message).to.equal(invalidQueryMessage);
                             done();
                         } catch (e) {
                             done(e);
@@ -131,6 +130,38 @@ function testFunc() {
                 }).catch(done);
             });
           });
-        });
+
+          describe('count()', function () {
+            it('should throw an error for an invalid query', (done) => {
+                store.count({})
+                .subscribe(null, (error) => {
+                    try {
+                        expect(error.message).to.equal(invalidQueryMessage);
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+            });
+        
+            it('should return the count for the collection', (done) => {
+                return store.count().toPromise()
+                .then((count) => {
+                    expect(count).to.equal(2);
+                    done();
+                }).catch(done);
+            });
+
+            it('should return the count of the entities that match the query', (done) => {
+                const query = new Kinvey.Query();
+                query.equalTo('_id', entity2._id);
+                return store.count(query).toPromise()
+                .then((count) => {
+                    expect(count).to.equal(1);
+                    done();
+                }).catch(done);
+            });
+          });
+
     });
 }
