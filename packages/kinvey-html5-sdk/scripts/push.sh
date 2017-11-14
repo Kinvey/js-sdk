26de9cb8d4178f150b8a7cf889fa19296f80c60d
Thomas Conner <thomas@kinvey.com>
2017-11-14 14:11:03 -0500

    Fix push script for kinvey-html5-sdk

--- packages/kinvey-html5-sdk/scripts/push.sh
+++ packages/kinvey-html5-sdk/scripts/push.sh
@@ -2,12 +2,12 @@
 
 # This script updates the Kinvey HTML5 SDK Github Repo with the latest release
 
-cd ../dist
-git init
+dist=../dist
+git init $dist
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
-git add .
-git commit -m "Travis Build: $TRAVIS_BUILD_NUMBER"
-git remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/Kinvey/html5-sdk.git > /dev/null 2>&1
-git tag $TRAVIS_TAG
-git push --tags --quiet --set-upstream origin master
+git -C $dist add .
+git -C $dist commit -m "Travis Build: $TRAVIS_BUILD_NUMBER"
+git -C $dist remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/Kinvey/html5-sdk.git > /dev/null 2>&1
+git -C $dist tag $TRAVIS_TAG
+git -C $dist push --tags --quiet --set-upstream origin master
