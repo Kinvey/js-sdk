ece153fc8ec222c658b85073b365ae05b5f30cc3
Travis CI <travis@travis-ci.org>
2017-11-14 15:15:36 -0500

    Fix scripts

--- packages/kinvey-html5-sdk/scripts/push.sh
+++ packages/kinvey-html5-sdk/scripts/push.sh
@@ -3,11 +3,16 @@
 # This script updates the Kinvey HTML5 SDK Github Repo with the latest release
 
 dist="$(dirname "$0")/../dist"
-git init $dist
+tmp="$(dirname "$0")/../tmp"
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
-git -C $dist add .
-git -C $dist commit -m "Travis Build: $TRAVIS_BUILD_NUMBER"
-git -C $dist remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/Kinvey/html5-sdk.git > /dev/null 2>&1
-git -C $dist tag $TRAVIS_TAG
-git -C $dist push --tags --quiet --set-upstream -f origin master
+git clone https://${GITHUB_ACCESS_TOKEN}@github.com/Kinvey/html5-sdk.git $tmp > /dev/null 2>&1
+git -C $tmp remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/Kinvey/html5-sdk.git > /dev/null 2>&1
+git -C $tmp fetch origin
+git -C $tmp checkout master
+shopt -s dotglob
+cp $dist/* $tmp
+git -C $tmp add .
+git -C $tmp commit -m "Travis Build: $TRAVIS_BUILD_NUMBER"
+git -C $tmp tag $TRAVIS_TAG
+git -C $tmp push --tags --quiet --set-upstream origin master
