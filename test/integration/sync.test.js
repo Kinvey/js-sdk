8e46a81d3ece70baf65610f0673ac7f12d430563
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-11-20 10:38:36 +0200

    Added sync tests

---
+++ test/integration/sync.test.js
@@ -0,0 +1,125 @@
+runner.run(testFunc);
+
+function testFunc() {
+  const collectionName = externalConfig.collectionName;
+  const appKey = externalConfig.appKey;
+  const appSecret = externalConfig.appSecret;
+
+  const dataStoreTypes = [Kinvey.DataStoreType.Cache, Kinvey.DataStoreType.Sync];
+
+
+  dataStoreTypes.forEach((currentDataStoreType) => {
+    describe(`${currentDataStoreType} Sync Tests`, () => {
+
+      let networkStore;
+      let syncStore;
+      let cacheStore;
+      let storeToTest;
+      const dataStoreType = currentDataStoreType;
+      const entity1 = common.getSingleEntity(common.randomString());
+      const entity2 = common.getSingleEntity(common.randomString());
+      const entity3 = common.getSingleEntity(common.randomString());
+      const createdUserIds = [];
+
+      before((done) => {
+        Kinvey.init({
+          appKey: appKey,
+          appSecret: appSecret
+        });
+        Kinvey.User.logout()
+          .then(() => {
+            return Kinvey.User.signup()
+          })
+          .then((user) => {
+            createdUserIds.push(user.data._id);
+            //store for setup
+            networkStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
+            syncStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Sync);
+            cacheStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Cache);
+            //store to test
+            storeToTest = Kinvey.DataStore.collection(collectionName, dataStoreType);
+            done();
+          });
+      });
+
+      beforeEach((done) => {
+        common.cleanUpCollectionData(collectionName)
+          .then(() => {
+            return syncStore.save(entity1)
+          })
+          .then(() => {
+            return syncStore.save(entity2)
+          })
+          .then(() => {
+            return cacheStore.save(entity3)
+          })
+          .then(() => done())
+          .catch(done)
+      });
+
+      after((done) => {
+        Kinvey.User.logout()
+          .then(() => {
+            return Kinvey.User.signup()
+          })
+          .then(() => {
+            return common.deleteUsers(createdUserIds)
+          })
+          .then(() => {
+            return common.cleanUpCollectionData(collectionName)
+          })
+          .then(() => {
+            return Kinvey.User.logout()
+          })
+          .then(() => done())
+          .catch(done)
+      });
+
+      describe('Pending sync queue', () => {
+
+        it('pendingSyncCount() should return the count of the entities waiting to be synced', (done) => {
+          storeToTest.pendingSyncCount()
+            .then((count) => {
+              expect(count).to.equal(2);
+              done();
+            }).catch(done);
+        });
+
+        it('clearSync() should clear the pending sync queue', (done) => {
+          syncStore.clearSync()
+            .then(() => {
+              return storeToTest.pendingSyncCount()
+            })
+            .then((count) => {
+              expect(count).to.equal(0);
+              done();
+            }).catch(done);
+        });
+
+        it('pendingSyncEntities() should return only of the entities waiting to be synced', (done) => {
+          storeToTest.pendingSyncEntities()
+            .then((entities) => {
+              expect(entities.length).to.equal(2);
+              entities.forEach((entity) => {
+                expect(entity.collection).to.equal(externalConfig.collectionName);
+                expect(entity.state.operation).to.equal('PUT');
+                expect([entity1._id, entity2._id]).to.include(entity.entityId);
+              })
+              done();
+            }).catch(done);
+        });
+
+        it('pendingSyncEntities() should return 0 if there are no entities waiting to be synced', (done) => {
+          syncStore.clearSync()
+            .then(() => {
+              return storeToTest.pendingSyncEntities()
+            })
+            .then((entities) => {
+              expect(entities).to.be.an.empty.array;
+              done();
+            }).catch(done);
+        });
+      });
+    });
+  });
+}
