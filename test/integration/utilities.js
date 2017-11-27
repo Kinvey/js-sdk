b88ec9cb0a55eff39c334d87a575a3b8917d9303
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-11-27 15:06:20 +0200

    Fixed the indentation

--- test/integration/utilities.js
+++ test/integration/utilities.js
@@ -1,233 +1,233 @@
 (function () {
-  
-function uid(size = 10) {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (let i = 0; i < size; i += 1) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  function uid(size = 10) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < size; i += 1) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
   }
 
-  return text;
-}
-
-function randomString(size = 18, prefix = '') {
-  return `${prefix}${uid(size)}`;
-}
-
-function randomEmailAddress() {
-  return `${randomString()}@test.com`;
-}
-
-function getEntity(_id, textValue, numberValue, array) {
-  const entity = {
-    textField: textValue || randomString(),
-    numberField: numberValue || numberValue === 0 ? numberValue : Math.random(),
-    arrayField: array || [randomString(), randomString()]
-  };
-  if (_id) {
-    entity._id = _id;
+  function randomString(size = 18, prefix = '') {
+    return `${prefix}${uid(size)}`;
   }
-  return entity;
-}
 
-function saveEntities(collectionName, entities) {
-  const networkStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
-  const syncStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Sync);
-  return Promise.all(entities.map(entity => {
-    return networkStore.save(entity)
-  }))
-    .then(() => syncStore.pull())
-    .then(result => _.sortBy(deleteEntityMetadata(result), '_id'));
-}
+  function randomEmailAddress() {
+    return `${randomString()}@test.com`;
+  }
 
-function deleteUsers(userIds) {
-  return Promise.all(userIds.map(userId => {
-    return Kinvey.User.remove(userId, {
-      hard: true
-    })
-      .then(() => {
-        userIds.length = 0
+  function getEntity(_id, textValue, numberValue, array) {
+    const entity = {
+      textField: textValue || randomString(),
+      numberField: numberValue || numberValue === 0 ? numberValue : Math.random(),
+      arrayField: array || [randomString(), randomString()]
+    };
+    if (_id) {
+      entity._id = _id;
+    }
+    return entity;
+  }
+
+  function saveEntities(collectionName, entities) {
+    const networkStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
+    const syncStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Sync);
+    return Promise.all(entities.map(entity => {
+      return networkStore.save(entity)
+    }))
+      .then(() => syncStore.pull())
+      .then(result => _.sortBy(deleteEntityMetadata(result), '_id'));
+  }
+
+  function deleteUsers(userIds) {
+    return Promise.all(userIds.map(userId => {
+      return Kinvey.User.remove(userId, {
+        hard: true
       })
-  }));
-}
+        .then(() => {
+          userIds.length = 0
+        })
+    }));
+  }
 
-function assertEntityMetadata(arrayOfEntities) {
-  const entities = [].concat(arrayOfEntities);
-  entities.forEach((entity) => {
-    expect(entity._kmd.lmt).to.exist;
-    expect(entity._kmd.ect).to.exist;
-    expect(entity._acl.creator).to.exist;
-  });
-}
-
-function deleteEntityMetadata(arrayOfEntities) {
-  if (arrayOfEntities instanceof Array) {
-    arrayOfEntities.forEach((entity) => {
-      delete entity['_kmd'];
-      delete entity['_acl'];
+  function assertEntityMetadata(arrayOfEntities) {
+    const entities = [].concat(arrayOfEntities);
+    entities.forEach((entity) => {
+      expect(entity._kmd.lmt).to.exist;
+      expect(entity._kmd.ect).to.exist;
+      expect(entity._acl.creator).to.exist;
     });
   }
-  else {
-    delete arrayOfEntities['_kmd'];
-    delete arrayOfEntities['_acl'];
-  }
-  return arrayOfEntities;
-}
 
-function validateReadResult(dataStoreType, spy, cacheExpectedEntities, backendExpectedEntities, sortBeforeCompare) {
-  let firstCallArgs = spy.firstCall.args[0];
-  let secondCallArgs;
-  if (dataStoreType === Kinvey.DataStoreType.Cache) {
-    secondCallArgs = spy.secondCall.args[0];
-  }
-  if (!_.isNumber(cacheExpectedEntities) && [].concat(cacheExpectedEntities)[0].hasOwnProperty('_id')) {
-    deleteEntityMetadata(firstCallArgs);
-    if (sortBeforeCompare) {
-      firstCallArgs = _.sortBy(firstCallArgs, '_id');
-      cacheExpectedEntities = _.sortBy(cacheExpectedEntities, '_id');
-      backendExpectedEntities = _.sortBy(backendExpectedEntities, '_id');
+  function deleteEntityMetadata(arrayOfEntities) {
+    if (arrayOfEntities instanceof Array) {
+      arrayOfEntities.forEach((entity) => {
+        delete entity['_kmd'];
+        delete entity['_acl'];
+      });
     }
-    if (secondCallArgs) {
-      deleteEntityMetadata(secondCallArgs);
+    else {
+      delete arrayOfEntities['_kmd'];
+      delete arrayOfEntities['_acl'];
+    }
+    return arrayOfEntities;
+  }
+
+  function validateReadResult(dataStoreType, spy, cacheExpectedEntities, backendExpectedEntities, sortBeforeCompare) {
+    let firstCallArgs = spy.firstCall.args[0];
+    let secondCallArgs;
+    if (dataStoreType === Kinvey.DataStoreType.Cache) {
+      secondCallArgs = spy.secondCall.args[0];
+    }
+    if (!_.isNumber(cacheExpectedEntities) && [].concat(cacheExpectedEntities)[0].hasOwnProperty('_id')) {
+      deleteEntityMetadata(firstCallArgs);
       if (sortBeforeCompare) {
-        secondCallArgs = _.sortBy(secondCallArgs, '_id');
+        firstCallArgs = _.sortBy(firstCallArgs, '_id');
+        cacheExpectedEntities = _.sortBy(cacheExpectedEntities, '_id');
+        backendExpectedEntities = _.sortBy(backendExpectedEntities, '_id');
+      }
+      if (secondCallArgs) {
+        deleteEntityMetadata(secondCallArgs);
+        if (sortBeforeCompare) {
+          secondCallArgs = _.sortBy(secondCallArgs, '_id');
+        }
       }
     }
-  }
 
-  if (dataStoreType === Kinvey.DataStoreType.Network) {
-    expect(spy.calledOnce).to.be.true;
-    expect(firstCallArgs).to.deep.equal(backendExpectedEntities);
-  }
-  else if (dataStoreType === Kinvey.DataStoreType.Sync) {
-    expect(spy.calledOnce).to.be.true;
-    expect(firstCallArgs).to.deep.equal(cacheExpectedEntities);
-  }
-  else {
-    expect(spy.calledTwice).to.be.true;
-    expect(firstCallArgs).to.deep.equal(cacheExpectedEntities);
-    expect(secondCallArgs).to.deep.equal(backendExpectedEntities);
-  }
-}
-
-function retrieveEntity(collectionName, dataStoreType, entity, searchField) {
-
-  const store = Kinvey.DataStore.collection(collectionName, dataStoreType);
-  const query = new Kinvey.Query();
-  const propertyToSearchBy = searchField || '_id';
-  query.equalTo(propertyToSearchBy, entity[propertyToSearchBy]);
-  return store.find(query).toPromise()
-    .then(result => result[0])
-}
-
-function validatePendingSyncCount(dataStoreType, collectionName, itemsForSyncCount, done) {
-  if (dataStoreType !== Kinvey.DataStoreType.Network) {
-    let expectedCount = 0;
-    if (dataStoreType === Kinvey.DataStoreType.Sync) {
-      expectedCount = itemsForSyncCount;
+    if (dataStoreType === Kinvey.DataStoreType.Network) {
+      expect(spy.calledOnce).to.be.true;
+      expect(firstCallArgs).to.deep.equal(backendExpectedEntities);
     }
+    else if (dataStoreType === Kinvey.DataStoreType.Sync) {
+      expect(spy.calledOnce).to.be.true;
+      expect(firstCallArgs).to.deep.equal(cacheExpectedEntities);
+    }
+    else {
+      expect(spy.calledTwice).to.be.true;
+      expect(firstCallArgs).to.deep.equal(cacheExpectedEntities);
+      expect(secondCallArgs).to.deep.equal(backendExpectedEntities);
+    }
+  }
+
+  function retrieveEntity(collectionName, dataStoreType, entity, searchField) {
+
     const store = Kinvey.DataStore.collection(collectionName, dataStoreType);
-    return store.pendingSyncCount()
-      .then((syncCount) => {
-        expect(syncCount).to.equal(expectedCount);
-        done();
-      }).catch(done);
+    const query = new Kinvey.Query();
+    const propertyToSearchBy = searchField || '_id';
+    query.equalTo(propertyToSearchBy, entity[propertyToSearchBy]);
+    return store.find(query).toPromise()
+      .then(result => result[0])
   }
-  else {
-    done();
-  }
-}
 
-function validateEntity(dataStoreType, collectionName, expectedEntity, searchField) {
-  return new Promise((resolve, reject) => {
-    let entityFromCache;
-    let entityFromBackend;
-
-    return retrieveEntity(collectionName, Kinvey.DataStoreType.Sync, expectedEntity, searchField)
-      .then((result) => {
-        if (result) {
-          entityFromCache = deleteEntityMetadata(result);
-        }
-        return retrieveEntity(collectionName, Kinvey.DataStoreType.Network, expectedEntity, searchField)
-      })
-      .then((result) => {
-        if (result) {
-          entityFromBackend = deleteEntityMetadata(result);
-        }
-        if (dataStoreType === Kinvey.DataStoreType.Network) {
-          expect(entityFromCache).to.be.undefined
-          expect(entityFromBackend).to.deep.equal(expectedEntity);
-        }
-        else if (dataStoreType === Kinvey.DataStoreType.Sync) {
-          expect(entityFromCache).to.deep.equal(expectedEntity);
-          expect(entityFromBackend).to.be.undefined
-        }
-        else {
-          expect(entityFromCache).to.deep.equal(expectedEntity);
-          expect(entityFromBackend).to.deep.equal(expectedEntity);
-        }
-        resolve();
-      }).catch(reject);
-  });
-}
-
-function cleanUpCollectionData(collectionName) {
-  const networkStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
-  const syncStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Sync);
-  return networkStore.find().toPromise()
-    .then((entities) => {
-      if (entities && entities.length > 0) {
-        const query = new Kinvey.Query();
-        query.contains('_id', entities.map(a => a._id));
-        return networkStore.remove(query)
+  function validatePendingSyncCount(dataStoreType, collectionName, itemsForSyncCount, done) {
+    if (dataStoreType !== Kinvey.DataStoreType.Network) {
+      let expectedCount = 0;
+      if (dataStoreType === Kinvey.DataStoreType.Sync) {
+        expectedCount = itemsForSyncCount;
       }
-    })
-    .then(() => {
-      return syncStore.clearSync()
-    })
-    .then(() => {
-      return syncStore.clear()
-    })
-}
+      const store = Kinvey.DataStore.collection(collectionName, dataStoreType);
+      return store.pendingSyncCount()
+        .then((syncCount) => {
+          expect(syncCount).to.equal(expectedCount);
+          done();
+        }).catch(done);
+    }
+    else {
+      done();
+    }
+  }
 
-function cleanUpAppData(collectionName, createdUserIds) {
-  return Kinvey.User.logout()
-    .then(() => {
-      return Kinvey.User.signup()
-    })
-    .then((user) => {
-      createdUserIds.push(user.data._id);
-      return cleanUpCollectionData(collectionName)
-    })
-    .then(() => {
-      return deleteUsers(createdUserIds)
-    })
-    .then(() => {
-      return Kinvey.User.logout()
-    })
-}
+  function validateEntity(dataStoreType, collectionName, expectedEntity, searchField) {
+    return new Promise((resolve, reject) => {
+      let entityFromCache;
+      let entityFromBackend;
 
-const utilities = {
-  uid,
-  randomString,
-  randomEmailAddress,
-  getEntity,
-  saveEntities,
-  deleteUsers,
-  assertEntityMetadata,
-  deleteEntityMetadata,
-  validateReadResult,
-  retrieveEntity,
-  validatePendingSyncCount,
-  validateEntity,
-  cleanUpCollectionData,
-  cleanUpAppData
-};
+      return retrieveEntity(collectionName, Kinvey.DataStoreType.Sync, expectedEntity, searchField)
+        .then((result) => {
+          if (result) {
+            entityFromCache = deleteEntityMetadata(result);
+          }
+          return retrieveEntity(collectionName, Kinvey.DataStoreType.Network, expectedEntity, searchField)
+        })
+        .then((result) => {
+          if (result) {
+            entityFromBackend = deleteEntityMetadata(result);
+          }
+          if (dataStoreType === Kinvey.DataStoreType.Network) {
+            expect(entityFromCache).to.be.undefined
+            expect(entityFromBackend).to.deep.equal(expectedEntity);
+          }
+          else if (dataStoreType === Kinvey.DataStoreType.Sync) {
+            expect(entityFromCache).to.deep.equal(expectedEntity);
+            expect(entityFromBackend).to.be.undefined
+          }
+          else {
+            expect(entityFromCache).to.deep.equal(expectedEntity);
+            expect(entityFromBackend).to.deep.equal(expectedEntity);
+          }
+          resolve();
+        }).catch(reject);
+    });
+  }
 
-if (typeof module === 'object') {
-  module.exports = utilities;
-} else {
-  window.utilities = utilities;
-}
+  function cleanUpCollectionData(collectionName) {
+    const networkStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Network);
+    const syncStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Sync);
+    return networkStore.find().toPromise()
+      .then((entities) => {
+        if (entities && entities.length > 0) {
+          const query = new Kinvey.Query();
+          query.contains('_id', entities.map(a => a._id));
+          return networkStore.remove(query)
+        }
+      })
+      .then(() => {
+        return syncStore.clearSync()
+      })
+      .then(() => {
+        return syncStore.clear()
+      })
+  }
+
+  function cleanUpAppData(collectionName, createdUserIds) {
+    return Kinvey.User.logout()
+      .then(() => {
+        return Kinvey.User.signup()
+      })
+      .then((user) => {
+        createdUserIds.push(user.data._id);
+        return cleanUpCollectionData(collectionName)
+      })
+      .then(() => {
+        return deleteUsers(createdUserIds)
+      })
+      .then(() => {
+        return Kinvey.User.logout()
+      })
+  }
+
+  const utilities = {
+    uid,
+    randomString,
+    randomEmailAddress,
+    getEntity,
+    saveEntities,
+    deleteUsers,
+    assertEntityMetadata,
+    deleteEntityMetadata,
+    validateReadResult,
+    retrieveEntity,
+    validatePendingSyncCount,
+    validateEntity,
+    cleanUpCollectionData,
+    cleanUpAppData
+  };
+
+  if (typeof module === 'object') {
+    module.exports = utilities;
+  } else {
+    window.utilities = utilities;
+  }
 })();
