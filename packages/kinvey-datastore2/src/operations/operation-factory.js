a30a9c7f47b3c2b43efce1f2fc58d1832afd16f3
Thomas Conner <thomas.conner@me.com>
2017-11-28 13:33:04 -0500

    Remove operations folder

--- packages/kinvey-datastore2/src/operations/operation-factory.js
+++
@@ -1,9 +0,0 @@
-import { Operation } from './operation';
-
-function getOperationWithData(type, collection, data) {
-  return new Operation(type, collection, null, data);
-}
-
-export const operationFactory = {
-  getOperationWithData
-};
--- packages/kinvey-datastore2/src/operations/operation-type.js
+++
@@ -1,3 +0,0 @@
-export const OperationType = {
-  Create: 'create'
-};
