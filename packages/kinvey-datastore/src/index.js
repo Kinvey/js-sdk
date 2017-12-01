707cefe67fd88b02c8e8e8498a5ac53cd6697ccb
Georgi Prodanov <georgi.prodanov@progress.com>
2017-12-01 13:25:47 +0200

    MLIBZ-2197 Add repositories.

---
+++ packages/kinvey-datastore/src/index.js
@@ -0,0 +1,7 @@
+export * from './datastores';
+export * from './operations';
+export * from './processors';
+export * from './repositories';
+// export * from './persisters';
+// export * from './sync';
+export * from './utils';
