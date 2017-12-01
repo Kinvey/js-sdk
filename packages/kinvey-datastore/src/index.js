de293b6840b15a91a07e3e03eda2ba7028faeb32
Georgi Prodanov <georgi.prodanov@progress.com>
2017-12-01 13:31:04 +0200

    MLIBZ-2197 Add persisters.

--- packages/kinvey-datastore/src/index.js
+++ packages/kinvey-datastore/src/index.js
@@ -2,6 +2,6 @@ export * from './datastores';
 export * from './operations';
 export * from './processors';
 export * from './repositories';
-// export * from './persisters';
+export * from './persisters';
 // export * from './sync';
 export * from './utils';
