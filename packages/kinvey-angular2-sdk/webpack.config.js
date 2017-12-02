405604198e09d21b5ea7009d2f05a1f1886fd5d8
Thomas Conner <thomas.conner@me.com>
2017-12-02 18:41:39 -0500

    Move kinvey-angular2-sdk package

---
+++ packages/kinvey-angular2-sdk/webpack.config.js
@@ -0,0 +1,108 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const fs = require('fs');
+const pkg = require('./package.json');
+
+module.exports = (env = {}) => {
+  return {
+    entry: {
+      'kinvey-angular2-sdk': './index.js',
+    },
+    externals: {
+      angular: 'angular'
+    },
+    output: {
+      filename: '[name].js',
+      pathinfo: true,
+      path: path.join(__dirname, 'dist'),
+      libraryTarget: 'umd',
+      library: 'Kinvey'
+    },
+    resolve: {
+      extensions: ['.js', '.json']
+    },
+    devtool: 'source-map',
+    module: {
+      rules: getRules()
+    },
+    plugins: getPlugins(env)
+  };
+}
+
+function getRules() {
+  return [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          sourceMaps: true
+        }
+      }
+    },
+    {
+      test: /\.json$/,
+      loaders: [
+        'json-loader'
+      ]
+    }
+  ];
+}
+
+function getPlugins(env) {
+  let plugins = [
+    // Copy assets to out dir. Add your own globs as needed.
+    new CopyWebpackPlugin([
+      {
+        from: 'package.json',
+        transform: (content) => {
+          const pkg = JSON.parse(content.toString('utf8'));
+          delete pkg.private;
+          delete pkg.devDependencies;
+          delete pkg.scripts;
+          return new Buffer(JSON.stringify(pkg, null, 2));
+        }
+      },
+      { from: path.join(__dirname, '../../src/kinvey.d.ts') },
+      { from: 'bower.json' },
+      { from: '.travis.yml' },
+      { from: 'LICENSE' },
+      { from: 'README.md' }
+    ])
+  ];
+
+  if (env.uglify) {
+    plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+
+    // Work around an Android issue by setting compress = false
+    plugins.push(new UglifyJSPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        mangle: { reserved: mangleExcludes },
+        output: {
+          comments: false
+        }
+      }
+    }));
+  }
+
+  plugins.push(new webpack.BannerPlugin({
+    banner: `
+/**
+ * ${pkg.name} - ${pkg.description}
+ * @version v${pkg.version}
+ * @author ${pkg.author}
+ * @link ${pkg.homepage}
+ * @license ${pkg.license}
+ */
+      `.trim(),
+    raw: true,
+    entryOnly: true
+  }));
+
+  return plugins;
+}
+
