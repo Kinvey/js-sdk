0e9304ac209b8481c50978a5c351344b23666644
Thomas Conner <thomas.conner@me.com>
2017-12-03 11:59:16 -0500

    Move persisters to src

--- packages/kinvey-datastore/src/index.js
+++
@@ -1,7 +0,0 @@
-export * from './datastores';
-export * from './operations';
-export * from './processors';
-export * from './repositories';
-export * from './persisters';
-// export * from './sync';
-export * from './utils';
--- packages/kinvey-datastore/src/repositories/storage-type.js
+++
@@ -1,3 +0,0 @@
-export const storageType = {
-  inmemory: 'inmemory'
-};
