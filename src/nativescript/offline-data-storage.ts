9a6579fbd7d540616c557a70f5cc0b44b6eab064
Georgi Prodanov <georgi.prodanov@progress.com>
2018-01-24 14:25:38 +0200

    MLIBZ-2273 Refactor SqliteKeyValueStorePersister into a generic SqlKeyValuePersister, which works a specific SQL module. Refactor WebSqlKeyValueStorePersister to be an SQL module. Now WebSQL and SQLite both use the same persister, with different SQL modules.

--- src/nativescript/offline-data-storage.ts
+++ src/nativescript/offline-data-storage.ts
@@ -2,14 +2,14 @@ import {
   repositoryProvider,
   storageType,
   KeyValueStoreOfflineRepository,
-  SqliteKeyValueStorePersister
+  SqlKeyValueStorePersister
 } from '../core/datastore';
 import { Client } from './client';
-import { NativescriptSqliteModule } from './nativescript-sqlite-module';
+import { NativescriptSqlModule } from './nativescript-sql-module';
 
 const sqliteStorageBuilder = (queue) => {
-  const sqlModule = new NativescriptSqliteModule(Client.sharedInstance().appKey);
-  const persister = new SqliteKeyValueStorePersister(sqlModule);
+  const sqlModule = new NativescriptSqlModule(Client.sharedInstance().appKey);
+  const persister = new SqlKeyValueStorePersister(sqlModule);
   return new KeyValueStoreOfflineRepository(persister, queue);
 };
 
