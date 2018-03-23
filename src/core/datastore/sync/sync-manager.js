e580555ed198364309fedf9e13f4b61fa5c5abc5
Thomas Conner <thomas.conner@progress.com>
2018-03-23 08:49:32 -0400

    Don't duplicate function calls in SyncManager.pull()

--- src/core/datastore/sync/sync-manager.js
+++ src/core/datastore/sync/sync-manager.js
@@ -77,13 +77,14 @@ export class SyncManager {
       return this._paginatedPull(collection, query, options);
     }
 
-    if (options.useDeltaSet) {
-      return this._fetchItemsFromServer(collection, query, options)
-        .then(entities => entities.length);
-    }
-
     return this._fetchItemsFromServer(collection, query, options)
-      .then(entities => this._replaceOfflineEntities(collection, query, entities))
+      .then((entities) => {
+        if (options.useDeltaSet) {
+          return this._replaceOfflineEntities(collection, query, entities);
+        }
+
+        return entities;
+      })
       .then(replacedEntities => replacedEntities.length);
   }
 
