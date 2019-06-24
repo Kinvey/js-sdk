1da667d97e826c66f645d2ce74de7341b1b1c781
Thomas Conner <thomas.conner@me.com>
2019-06-24 14:50:40 -0400

    Working on MIC

---
+++ packages/js-sdk/src/identity/login.ts
@@ -0,0 +1,35 @@
+import { KinveyError } from '../errors';
+import {
+  KinveyHttpRequest,
+  HttpRequestMethod,
+  formatKinveyBaasUrl,
+  KinveyBaasNamespace,
+  kinveyAppAuth
+} from '../http';
+import { setSession } from '../session';
+import { User, UserData, getActiveUser } from '../user';
+
+export async function login<T extends UserData>(credentials: object): Promise<User<T>> {
+  const activeUser = getActiveUser();
+
+  if (activeUser) {
+    throw new KinveyError('An active user already exists. Please logout the active user before you login.');
+  }
+
+  const request = new KinveyHttpRequest({
+    method: HttpRequestMethod.POST,
+    auth: kinveyAppAuth,
+    url: formatKinveyBaasUrl(KinveyBaasNamespace.User, '/login'),
+    body: credentials
+  });
+  const response = await request.execute();
+  const session = response.data;
+
+  // Remove sensitive data
+  delete session.password;
+
+  // Store the active session
+  setSession(session);
+
+  return new User<T>(session);
+}
---
+++ packages/js-sdk/src/identity/loginWithRedirectUri.ts
@@ -0,0 +1,44 @@
+import isString from 'lodash/isString';
+import { KinveyError } from '../errors';
+import { getAppKey } from '../init';
+import { login } from './login';
+// import { loginWithPopup, getTokenWithCode } from './mic';
+// import { signup } from './signup';
+import { User, UserData, getActiveUser, signup } from '../user';
+
+export interface MICOptions {
+  micId?: string;
+  version?: string | number;
+  timeout?: number;
+}
+
+export async function loginWithRedirectUri<T extends UserData>(redirectUri: string, options: MICOptions = {}): Promise<User<T>> {
+  const activeUser = getActiveUser();
+  let clientId = getAppKey();
+
+  if (activeUser) {
+    throw new KinveyError('An active user already exists. Please logout the active user before you login.');
+  }
+
+  if (!isString(redirectUri)) {
+    throw new KinveyError('A redirectUri is required and must be a string.');
+  }
+
+  if (isString(options.micId)) {
+    clientId = `${clientId}.${options.micId}`;
+  }
+
+  const code = await loginWithPopup(clientId, redirectUri, version);
+  const token = await getTokenWithCode(code, clientId, redirectUri, options);
+  const credentials = { _socialIdentity: { [token.identity]: token } };
+
+  try {
+    return await login(credentials);
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      return signup(credentials);
+    }
+
+    throw error;
+  }
+}
