84d2b33c75879bc741e005cffca59b1f390a5b6b
Thomas Conner <thomas.conner@me.com>
2019-07-25 11:28:18 -0400

    Add endpoint

---
+++ packages/node-sdk/src/endpoint.ts
@@ -0,0 +1,24 @@
+import isString from 'lodash/isString';
+import { KinveyError } from './errors';
+import {
+  kinveySessionOrAppAuth,
+  formatKinveyBaasUrl,
+  KinveyBaasNamespace,
+  KinveyHttpRequest,
+  HttpRequestMethod
+} from './http';
+
+export async function endpoint(endpointPath: string, args?: any): Promise<any> {
+  if (!isString(endpointPath)) {
+    throw new KinveyError('You must provide an endpoint as a string.');
+  }
+
+  const request = new KinveyHttpRequest({
+    method: HttpRequestMethod.POST,
+    auth: kinveySessionOrAppAuth,
+    url: formatKinveyBaasUrl(KinveyBaasNamespace.Rpc, `/custom/${endpointPath}`),
+    body: args
+  });
+  const response = await request.execute();
+  return response.data;
+}
