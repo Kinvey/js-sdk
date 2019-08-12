78205d1570d78d280cea0f32d0fba77607bafde8
Thomas Conner <thomas.conner@me.com>
2019-08-12 11:25:44 -0400

    feat(react-native-sdk): add sqlite as a storage adapter

--- packages/react-native-sdk/src/storage/index.ts
+++ packages/react-native-sdk/src/storage/index.ts
@@ -1,14 +1,18 @@
-import { Errors } from 'kinvey-js-sdk';
-import * as Memory from './memory';
+import { Errors } from "kinvey-js-sdk";
+import * as Memory from "./memory";
+import * as SQLite from "./sqlite";
 
 export enum StorageProvider {
-  Memory = 'Memory'
-};
+  Memory = "Memory",
+  SQLite = "SQLite"
+}
 
-export function getStorageAdapter(storageProvider = StorageProvider.Memory) {
+export function getStorageAdapter(storageProvider = StorageProvider.SQLite) {
   if (storageProvider === StorageProvider.Memory) {
     return Memory;
+  } else if (storageProvider === StorageProvider.SQLite) {
+    return SQLite;
   }
 
-  throw new Errors.KinveyError('You must override the default cache store.');
+  throw new Errors.KinveyError("You must override the default cache store.");
 }
