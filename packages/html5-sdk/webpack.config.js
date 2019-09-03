b75f79467097b2df3fb912931ad2a07ae665a688
Thomas Conner <thomas.conner@me.com>
2019-09-03 15:03:39 -0400

    chore: fix import of clean-webpack-plungin

--- packages/html5-sdk/webpack.config.js
+++ packages/html5-sdk/webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const pkg = require('./package.json');
 
 const BANNER = `
