56bfae4b35e0ba14bddbc76ce1527201a358282a
Thomas Conner <thomas.conner@me.com>
2019-09-03 14:35:09 -0400

    chore: remove husky commit-msg hook
    This hook prevents lerna from complete the publish task.

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   hooks: {
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook'
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
   }
 };
