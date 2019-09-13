0ad66e3e98b5e034c5337e5aa3b1ab14815049d7
Thomas Conner <thomas.conner@me.com>
2019-09-13 10:57:50 -0400

    refactor: remove old tests

--- tests/integration/html5/webpack.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // Source maps support ('inline-source-map' also works)
-  devtool: 'source-map'
-};
--- tests/unit/user/utils.js
+++
@@ -1,9 +0,0 @@
-export function mergeSocialIdentity(origSocialIdentity = {}, newSocialIdentity = {}) {
-  const _origSocialIdentity = JSON.parse(JSON.stringify(origSocialIdentity));
-  const _newSocialIdentity = JSON.parse(JSON.stringify(newSocialIdentity));
-  const result = Object.keys(_newSocialIdentity).reduce((socialIdentity, identity) => {
-    socialIdentity[identity] = Object.assign(_newSocialIdentity[identity], _origSocialIdentity[identity]);
-    return socialIdentity;
-  }, _newSocialIdentity);
-  return result;
-}
