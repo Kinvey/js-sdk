7d07a3ed3b8309f528f3b61bfc0271a3841eb536
filipov <filipov@progress.com>
2021-05-27 15:40:23 +0300

    Use single copy-specs script

--- tests/integration/copy-specs.js
+++ tests/integration/copy-specs.js
@@ -1,12 +1,17 @@
+const argv = require('yargs').argv;
 const isArray = require('lodash/isArray');
 const fs = require('fs-extra');
 const path = require('path');
 const glob = require('glob-promise');
 const babel = require('@babel/core');
-require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
-const SPECS_DIR = path.join(__dirname, '../../specs');
-const TEST_DIR = path.join(__dirname, '../test');
+if (!argv.target) {
+  throw new Error('Required parameter "--target" missing.');
+}
+
+const SPECS_DIR = path.join(__dirname, './specs');
+const TEST_DIR = path.join(__dirname, `./${argv.target}/test`);
 
 function build(file) {
   let singular = false;
@@ -31,7 +36,14 @@ function copy(files) {
     .then(function (results) {
       return Promise.all(results
         .map(function (result) {
-          result.code = result.code.replace('__SDK__', 'kinvey-node-sdk');
+          result.code = result.code.replace('__SDK__', `kinvey-${argv.target}-sdk`);
+
+          if (argv.runner === 'jest') {
+            // Adjust mocha-style `before`s to jest syntax
+            result.code = result.code.replace(/before\(/g, 'beforeAll(');
+            result.code = result.code.replace(/after\(/g, 'afterAll(');
+          }
+
           return result;
         })
         .map(function (result) {
@@ -46,7 +58,7 @@ fs.remove(TEST_DIR)
     return []
       .concat(glob.sync(path.join(SPECS_DIR, '*.js')))
       .concat(glob.sync(path.join(SPECS_DIR, 'common/**/*.js')))
-      .concat(glob.sync(path.join(SPECS_DIR, 'node/**/*.js')));
+      .concat(glob.sync(path.join(SPECS_DIR, `${argv.target}/**/*.js`)));
   })
   .then((files) => copy(files))
   .catch(function (error) {
