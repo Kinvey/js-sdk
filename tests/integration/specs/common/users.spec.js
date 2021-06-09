285cc137a88168361df3d44eedeadae7b82e6ba4
filipov <filipov@progress.com>
2021-06-09 18:03:37 +0300

    Adjust test hooks to be jest-compatible

--- tests/integration/specs/common/users.spec.js
+++ tests/integration/specs/common/users.spec.js
@@ -174,12 +174,13 @@ describe('User tests', () => {
       let username;
       let password;
 
-      before('setup user with MFA', async () => {
+      // setup user with MFA
+      before(async () => {
         ({ createdUser, userAuthenticator, username, password } = await utilities.setupUserWithMFA(appCredentials, true));
         createdUserIds.push(createdUser.data._id);
       });
 
-      after('cleanup authenticator', async () => utilities.removeAuthenticator(createdUser.data._id, userAuthenticator.id, appCredentials));
+      after(async () => utilities.removeAuthenticator(createdUser.data._id, userAuthenticator.id, appCredentials));
 
       describe('login()', () => {
         it('should throw an error', async () => {
@@ -261,7 +262,7 @@ describe('User tests', () => {
         });
 
         describe('when an active user already exists', () => {
-          before('setup active user', async () => {
+          before(async () => {
             const existingActiveUser = await Kinvey.User.signup();
             createdUserIds.push(existingActiveUser.data._id);
           });
@@ -277,13 +278,14 @@ describe('User tests', () => {
           let gullibleUserAuthenticator;
           let gullibleUser;
 
-          before('setup new user with MFA', async () => {
+          // setup new user with MFA
+          before(async () => {
             ({ createdUser: gullibleUser, userAuthenticator: gullibleUserAuthenticator, username: gullibleUserName, password: gullibleUserPassword } =
               await utilities.setupUserWithMFA(appCredentials, true));
             createdUserIds.push(gullibleUser.data._id);
           });
 
-          after('cleanup authenticator', async () => utilities.removeAuthenticator(gullibleUser.data._id, gullibleUserAuthenticator.id, appCredentials));
+          after(async () => utilities.removeAuthenticator(gullibleUser.data._id, gullibleUserAuthenticator.id, appCredentials));
 
           it('should not ask the same user for MFA code on second login', async () => {
             const selectAuthenticator = (authenticators) => (authenticators.find((a) => a.id === gullibleUserAuthenticator.id).id);
@@ -369,7 +371,7 @@ describe('User tests', () => {
         });
 
         describe('when an active user already exists', () => {
-          before('setup active user', async () => {
+          before(async () => {
             const existingActiveUser = await Kinvey.User.signup();
             createdUserIds.push(existingActiveUser.data._id);
           });
@@ -428,7 +430,8 @@ describe('User tests', () => {
     const username = utilities.randomString();
     const password = utilities.randomString();
 
-    before('setup data store and user', async () => {
+    // setup data store and user
+    before(async () => {
       syncDataStore = Kinvey.DataStore.collection(collectionName, Kinvey.DataStoreType.Sync);
       await safelySignUpUser(username, password, true, createdUserIds);
       if (!(await Kinvey.User.getActiveUser())) {
