b78dfac6c2b2ef1574739a5a139247a91f14b728
Martin Apostolov <martin.apostolov@progress.com>
2022-04-27 09:56:22 +0300

    fix: update webpack configuration to polyfill Buffer
    Webpack v5 no longer polyfill node.js APIs including Buffer.
    Therefore added Buffer library polyfill and
    configured Webpack to leverage the provided package as a “fallback” for APIs
    which are not natively available in the target environment (browser).
    The Webpack ProvidePlugin provides a mechanism for injecting a module/value
    as a replacement/API for global variables

--- tests/integration/html5/webpack.config.js
+++ tests/integration/html5/webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   // Source maps support ('inline-source-map' also works)
   devtool: 'source-map',
@@ -5,4 +7,17 @@ module.exports = {
 	optimization: {
 		minimize: false
 	},
+  resolve: {
+    fallback: {
+        buffer: require.resolve('buffer/'),
+    },
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+      }),
+  ],
+  stats: {
+    errorDetails: true,
+  },
 };
