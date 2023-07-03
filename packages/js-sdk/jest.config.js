ad7e5ad56b5efc44459192524edfbdeddc0c4531
PROGRESS\dedimitr <denitsa.dimitrova@progress.com>
2023-07-03 12:55:06 +0300

    CHAT-4575: Fix unit tests; skip some

--- packages/js-sdk/jest.config.js
+++ packages/js-sdk/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   collectCoverage: true,
   transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest',
-  }
+  },
+  transformIgnorePatterns: ['node_modules\\/(?!(lodash-es)\\/)'],
+  testMatch: ['<rootDir>/test/*.spec.ts'],
 };
